8423ea95988272cb34222e94b4c039cd3fab1080
ast_ostankevych <o.stankevych@astoundcommerce.com>
2021-12-06 17:59:25 +0200

    add mobile layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+document.addEventListener("DOMContentLoaded", function(event) { 
+    burger = document.getElementById("burger");
+    navigation = document.getElementById("navigation");
+    burger.addEventListener("click", function() {
+        if (burger.classList.contains("active")) {
+            burger.classList.remove("active");
+            navigation.style.display = "none";            
+        } else {
+            burger.classList.add("active");
+            navigation.style.display = "block";
+        }        
+    }, false);
+});  
